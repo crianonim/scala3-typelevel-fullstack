4bb15516a70c8898d5336a26dcad74e69dacfd08
Jan Skowronski <crianonim@gmail.com>
2025-04-21 10:09:29 +0100

    add circe

--- scala-cli/Tapir-cats-effect.scala
+++ scala-cli/Tapir-cats-effect.scala
@@ -2,6 +2,7 @@
 //> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.11.25
 //> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.11.25
 //> using dep org.http4s::http4s-blaze-server:0.23.17
+//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.11.25
 
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.syntax.all.*
@@ -11,8 +12,11 @@ import org.http4s.server.Router
 import sttp.tapir.*
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
+import sttp.tapir.json.circe.*
+
 
 object HelloWorldTapir extends IOApp:
+  
   val helloWorldEndpoint = endpoint.get
     .in("hello" / "world")
     .in(query[String]("name"))
@@ -21,15 +25,29 @@ object HelloWorldTapir extends IOApp:
       .println(s"Saying hello to: $name")
       .flatMap(_ => IO.pure(Right(s"Hello, $name!"))))
 
+  val calculationEndpoint = endpoint.get
+    .in("calculate" / "sum")
+    .in(query[Int]("a"))
+    .in(query[Int]("b"))
+    .out(jsonBody[Int])
+    .serverLogic[IO] { (a, b) =>
+      IO.pure(Right(a + b))
+    }
+
   val helloWorldRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]()
     .toRoutes(helloWorldEndpoint)
 
-  val swaggerEndpoints = SwaggerInterpreter()
-    .fromServerEndpoints[IO](List(helloWorldEndpoint), "My App", "1.0")
+  val calculationRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]()
+    .toRoutes(calculationEndpoint)
+
+    // This is the endpoint that will be used to generate the Swagger documentation
+    // if fromServerEndpoints requires an effect type (or Identity)
+    val swaggerEndpoints = SwaggerInterpreter()
+    .fromServerEndpoints[IO](List(helloWorldEndpoint, calculationEndpoint), "My App", "1.0")
 
   val swaggerRoutes: HttpRoutes[IO] = Http4sServerInterpreter[IO]().toRoutes(swaggerEndpoints)
 
-  val allRoutes: HttpRoutes[IO] = helloWorldRoutes <+> swaggerRoutes
+  val allRoutes: HttpRoutes[IO] = helloWorldRoutes <+> swaggerRoutes <+> calculationRoutes
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
