cf4a1c998602012702a555b35c38703262a71bcf
Jan Skowronski <crianonim@gmail.com>
2025-11-15 15:29:00 +0000

    viewportWidth controlled

--- app/src/main/scala/com/crianonim/timelines/TimelinesApp.scala
+++ app/src/main/scala/com/crianonim/timelines/TimelinesApp.scala
@@ -11,26 +11,45 @@ import com.crianonim.timelines.{Timeline, Viewport}
 object TimelinesApp {
   case class Model(
       timelines: List[Timeline],
-      viewport: Viewport
+      viewport: Viewport,
+      viewportWidth: Float
   )
 
   enum Msg {
     case Noop
+    case UpdateViewportWidth(value: String)
   }
 
   def init: Model = Model(
-      timelines = Timeline.examples,
-      viewport = Timeline.getViewportForTimelines(Timeline.examples)
+    timelines = Timeline.examples,
+    viewport = Timeline.getViewportForTimelines(Timeline.examples),
+    viewportWidth = 500
   )
 
-  def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = msg => (model, Cmd.None)
+  def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = {
+    case Msg.Noop => (model, Cmd.None)
+    case Msg.UpdateViewportWidth(value) =>
+      value.toFloatOption match {
+        case Some(width) if width > 0 => (model.copy(viewportWidth = width), Cmd.None)
+        case _                        => (model, Cmd.None)
+      }
+  }
 
   def view(model: Model): Html[Msg] = {
     div(cls := "flex flex-col gap-2 p-10")(
       div(cls := "flex gap-2 text-storm-dust-700 items-center")(
-        text("TimeLines")
+        div()(text("TimeLines")),
+        div(cls := "flex gap-2 items-center")(
+          div()(text("Viewport Width:")),
+          input(
+            `type` := "number",
+            value := model.viewportWidth.toString,
+            onInput(value => Msg.UpdateViewportWidth(value)),
+            cls := "border border-gray-300 rounded px-2 py-1 w-24"
+          )
+        )
       ),
-      div(cls := "flex flex-col gap-1 p-2")(model.timelines.map(viewTimeline(model.viewport))),
+      div(cls := "flex flex-col gap-1 p-2")(model.timelines.map(viewTimeline(model))),
       div(cls := "flex flex-col gap-1 p-2")(model.timelines.map(viewTimelineAsText))
     )
   }
@@ -80,11 +99,11 @@ object TimelinesApp {
       cls   := "bg-sky-500 h-4 border border-black"
     )()
   }
-  def viewTimeline(viewport: Viewport)(tl: Timeline) = {
+  def viewTimeline(model: Model)(tl: Timeline) = {
 
-    val bar = TimeLineBar.timelineToTimelineBar(viewport, 500, tl)
+    val bar = TimeLineBar.timelineToTimelineBar(model.viewport, model.viewportWidth, tl)
     div(cls := "flex gap-4 items-center")(
-      div(cls := "w-[500px]")(
+      div(cls := "w-[500px]", attribute("width", model.viewportWidth.toString ++ "px"))(
         viewBar(bar)
       )
     )
